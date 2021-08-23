84b6f3bf65bedc6d329c088b6d4f83fa6c87d4d7
HicaroD <hdanrlley1@gmail.com>
2021-08-23 15:39:10 -0300

    Improving overall GUI structure
    Improving the GUI design using some best practices for Tkinter
    - Creating functions for drawing the widgets
    - Creating function for configure the master window layout

--- scripts/main.py
+++ scripts/main.py
@@ -20,20 +20,23 @@ TODO:
 """
 
 
-class MusicPlayer(object):
-    def __init__(self):
+class MusicPlayer(tkinter.Frame):
+    def __init__(self, master = None):
         # Configurações do layout da janela
-        self.window = tkinter.Tk()
-        self.window.title("Music Player")
-        self.window.geometry("480x480")
+        tkinter.Frame.__init__(self, master)
+        self.master = master
+        self.configure_gui()
 
         # Setup padrão para o MusicPlayer funcionar
-        self.draw_buttons()
+        self.create_buttons()
         self.player_instance = vlc.Instance("--loop")
         self.current_playlist_path = tkinter.filedialog.askdirectory()
         self.music_player = self.create_playlist()
 
-        self.window.mainloop()
+    def configure_gui(self):
+        self.master.title("Music Player")
+        self.master.geometry("480x480")
+        self.master.resizable(False, False)
 
     def ask_for_playlist_path(self):
         # Ask for a new playlist to play
@@ -75,7 +78,7 @@ class MusicPlayer(object):
         print("Previous music")
         self.music_player.previous()
 
-    def draw_buttons(self):
+    def create_buttons(self):
         self.select_playlist_button()
         self.previous_music_button()
         self.pause_music_button()
@@ -83,25 +86,27 @@ class MusicPlayer(object):
         self.next_music_button()
     
     def select_playlist_button(self): 
-        playlist_select_btn = tkinter.Button(self.window, text="Open a playlist", fg="blue", command=self.ask_for_playlist_path)
+        playlist_select_btn = tkinter.Button(self.master, text="Open a playlist", fg="blue", command=self.ask_for_playlist_path)
         playlist_select_btn.grid(column = 0, row = 0)
 
     def play_music_button(self):
-        play_music_bttn = tkinter.Button(self.window, text="Play", command=self.play_music)
+        play_music_bttn = tkinter.Button(self.master, text="Play", command=self.play_music)
         play_music_bttn.grid(column = 1, row = 0)
 
     def pause_music_button(self):   
-        pause_bttn = tkinter.Button(self.window, text="Pause", command=self.pause_music)
+        pause_bttn = tkinter.Button(self.master, text="Pause", command=self.pause_music)
         pause_bttn.grid(column = 2, row = 0)
         
     def next_music_button(self):
-        next_music_btton = tkinter.Button(self.window, text="Next", command=self.next_music)
+        next_music_btton = tkinter.Button(self.master, text="Next", command=self.next_music)
         next_music_btton.grid(column = 3, row = 0)
 
     def previous_music_button(self):
-        previous_music_btton = tkinter.Button(self.window, text="Previous", command=self.previous_music)
+        previous_music_btton = tkinter.Button(self.master, text="Previous", command=self.previous_music)
         previous_music_btton.grid(column = 4, row = 0)
         
 
 if __name__ == "__main__": 
-    mainWindow = MusicPlayer()
+    root = tkinter.Tk()
+    music_player = MusicPlayer(root)
+    root.mainloop()
