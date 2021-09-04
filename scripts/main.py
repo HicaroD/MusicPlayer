3fce88198950e73afc7803d039c6a15fd58e21f3
HicaroD <hdanrlley1@gmail.com>
2021-09-04 17:39:47 -0300

    Deleting pause button and integrating it to the play button

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,10 @@
 import tkinter
-import vlc
-import os
 import tkinter.filedialog
+import os
+import vlc
 
 """
 TODO:
-    [X] Obter os árquivos de aúdio na pasta
-    [X] Tocar áudio
-    [X] Criar botão para escolher a playlist
-    [X] Fazer o player iterar sobre a lista de músicas
-    [X] Botão para tocar música
-    [X] Botão para pausar
-    [X] Botão para próxima música
-    [X] Botão para voltar para música anterior
-    [X] Consertar o botão de mudar playlist (que não afetava em nada na hora que tentava mudar a playlist)
-    [X] BUG: Quando inicio o programa, mas fecho a primeira aba de procurar playlist, se eu quiser colocar outra playlist o programa não funciona
-    [X] Obter as informações da música que está tocando atualmente
-    [] BUG: Quando eu altero a playlist a função de colocar música na tela para de funcionar
     [] Configurar o nome atual da música para atualizar automaticamente quando mudamos de música
     [] Personalizar melhor os botões (talvez usar imagens caso possível - Buscar images grátis)
     [] Adicionar funcionalidades de aumentar / diminuir o volume da música
@@ -32,18 +20,27 @@ class MusicPlayer(tkinter.Frame):
 
     def configure_gui(self):
         self.master.title("Music Player")
-        self.master.geometry("480x480")
+        self.master.geometry("300x300")
         self.master.resizable(False, False)
         self.create_buttons()
 
     def configure_mp3_player(self):
         self.player_instance = vlc.Instance("--loop")
-        self.current_playlist_path = None
         self.music_player = None
         self.current_music_name = self.get_current_music_name()
 
+    def create_buttons(self):
+        self.select_music_button()
+        self.select_playlist_button()
+        self.previous_music_button()
+        self.play_music_button()
+        self.next_music_button()
+
     def select_music(self):
         music_path = tkinter.filedialog.askopenfilename()
+        if not music_path.endswith(".mp3"):
+            raise Exception("Select an valid file -> mp3")
+
         if(self.music_player is not None and self.music_player.is_playing()):
             self.music_player.stop()
         print(f"Fetching {music_path} to music player")
@@ -88,13 +85,13 @@ class MusicPlayer(tkinter.Frame):
             print(f"Fetching {self.songs} playlist and adding to the playlist player")
             return playerList
 
-    def play_music(self):
-        print("Playing music")
-        self.music_player.play()
-
-    def pause_music(self):
-        print("Pausing music")
-        self.music_player.pause()
+    def play_and_pause_music(self):
+        if(self.music_player.is_playing()):
+            print("Pausing music")
+            self.music_player.pause()
+        else:
+            print("Playing music")
+            self.music_player.play()
 
     def next_music(self):
         print("Next music")
@@ -103,14 +100,6 @@ class MusicPlayer(tkinter.Frame):
     def previous_music(self):
         print("Previous music")
         self.music_player.previous()
-
-    def create_buttons(self):
-        self.select_music_button()
-        self.select_playlist_button()
-        self.previous_music_button()
-        self.pause_music_button()
-        self.play_music_button()
-        self.next_music_button()
 
     def select_music_button(self):
         select_music_bttn = tkinter.Button(self.master, text = "Select music", command = self.select_music)
@@ -121,12 +110,8 @@ class MusicPlayer(tkinter.Frame):
         playlist_select_btn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.NW)
 
     def play_music_button(self):
-        play_music_bttn = tkinter.Button(self.master, text="Play", command=self.play_music)
+        play_music_bttn = tkinter.Button(self.master, text="Play / Pause", command=self.play_and_pause_music)
         play_music_bttn.place(relx=0.5, rely=0.5, anchor=tkinter.constants.CENTER)
-
-    def pause_music_button(self):
-        pause_bttn = tkinter.Button(self.master, text="Pause", command=self.pause_music)
-        pause_bttn.place(relx=0.5, rely=0.6, anchor = tkinter.constants.CENTER)
 
     def next_music_button(self):
         next_music_btton = tkinter.Button(self.master, text="Next", command=self.next_music)
