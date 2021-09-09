5ed3c02a7c165bec3e65ce823a8789e72e112911
HicaroD <hdanrlley1@gmail.com>
2021-09-09 15:58:47 -0300

    Creating a make_button() wrapper

--- scripts/main.py
+++ scripts/main.py
@@ -31,24 +31,27 @@ class MusicPlayer(tkinter.Frame):
         self.play_music_button()
         self.next_music_button()
 
+    def make_button(self, text, target):
+        return tkinter.Button(self.master, text = text, command = target)
+
     def select_music_button(self):
-        select_music_bttn = tkinter.Button(self.master, text = "Select music", command = self.player.select_music)
-        select_music_bttn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.NE)
+        select_music_bttn = self.make_button("Select music", self.player.select_music)
+        select_music_bttn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
     def select_playlist_button(self):
-        playlist_select_btn = tkinter.Button(self.master, text="Open a playlist", fg="blue", command=self.player.ask_for_playlist_path)
-        playlist_select_btn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.NW)
+        playlist_select_btn = self.make_button("Open playlist", self.player.ask_for_playlist_path)
+        playlist_select_btn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
     def play_music_button(self):
-        play_music_bttn = tkinter.Button(self.master, text="Play / Pause", command=self.player.play_and_pause_music)
+        play_music_bttn = self.make_button("Play / Pause", self.player.play_and_pause_music)
         play_music_bttn.place(relx=0.5, rely=0.5, anchor=tkinter.constants.CENTER)
 
     def next_music_button(self):
-        next_music_btton = tkinter.Button(self.master, text="Next", command=self.player.next_music)
+        next_music_btton = self.make_button("Next", self.player.next_music)
         next_music_btton.pack(side = tkinter.constants.RIGHT)
 
     def previous_music_button(self):
-        previous_music_btton = tkinter.Button(self.master, text="Previous", command=self.player.previous_music)
+        previous_music_btton = self.make_button("Previous", self.player.previous_music)
         previous_music_btton.pack(side = tkinter.constants.LEFT)
 
 if __name__ == "__main__":
