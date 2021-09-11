7b0b13d1dd1c12869e7487fae00ec0db02bb90b1
HicaroD <hdanrlley1@gmail.com>
2021-09-11 14:58:19 -0300

    Creating a new class to manage the button creation

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 import tkinter
 import tkinter.filedialog
 import os
-from player import MusicPlayer
+from player import Player
 
 """
 TODO:
@@ -11,18 +11,17 @@ TODO:
     [] Configurar o nome atual da música para atualizar automaticamente quando mudamos de música
     [] Personalizar melhor os botões (talvez usar imagens caso possível - Buscar images grátis)
 """
-
 class Application(tkinter.Frame):
     def __init__(self, master = None):
         # Configurações do layout da janela
         tkinter.Frame.__init__(self, master)
         self.master = master
-        self.player = MusicPlayer()
+        self.player = Player()
         self.configure_gui()
 
     def configure_gui(self):
         self.master.title("Music Player")
-        self.master.geometry("300x300")
+        self.master.geometry("400x400")
         self.master.resizable(False, False)
         self.draw_buttons()
 
@@ -33,28 +32,31 @@ class Application(tkinter.Frame):
         self.play_music_button()
         self.next_music_button()
 
-    def make_button(self, text, target):
-        return tkinter.Button(self.master, text = text, command = target)
-
     def select_music_button(self):
-        select_music_bttn = self.make_button("Select music", self.player.select_music)
+        select_music_bttn = Button.make_button(self.master, "Select music", self.player.select_music)
         select_music_bttn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
     def select_playlist_button(self):
-        playlist_select_btn = self.make_button("Open playlist", self.player.ask_for_playlist_path)
+        playlist_select_btn = Button.make_button(self.master, "Open playlist", self.player.ask_for_playlist_path)
         playlist_select_btn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
     def play_music_button(self):
-        play_music_bttn = self.make_button("Play / Pause", self.player.play_and_pause_music)
+        play_music_bttn = Button.make_button(self.master, "Play/Pause", self.player.play_and_pause_music)
         play_music_bttn.place(relx=0.5, rely=0.5, anchor=tkinter.constants.CENTER)
 
     def next_music_button(self):
-        next_music_btton = self.make_button("Next", self.player.next_music)
+        next_music_btton = Button.make_button(self.master, "Next", self.player.next_music)
         next_music_btton.pack(side = tkinter.constants.RIGHT)
 
     def previous_music_button(self):
-        previous_music_button = self.make_button("Previous", self.player.previous_music)
-        previous_music_button.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.NE)
+        previous_music_button = Button.make_button(self.master, "Previous", self.player.previous_music)
+        previous_music_button.pack(side = tkinter.constants.LEFT)
+
+class Button:
+    # ps.: It's not part of tkinter.Button() class
+    @staticmethod
+    def make_button(master, text, target):
+        return tkinter.Button(master, text = text, command = target)
 
 if __name__ == "__main__":
     root = tkinter.Tk()
