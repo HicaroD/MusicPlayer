af763180b64076b00697475e7c9d85fe1a49ab07
HicaroD <hdanrlley1@gmail.com>
2021-09-15 12:33:30 -0300

    BUG: Segmentation fault (coredump) when cancelling the playlist selection (I don't know why)

--- scripts/main.py
+++ scripts/main.py
@@ -19,36 +19,36 @@ class Application(tkinter.Frame):
         self.player = MusicPlayer()
         self.configure_gui()
 
-    def configure_gui(self):
+    def configure_gui(self) -> None:
         self.master.title("Music Player")
         self.master.geometry("400x400")
         self.master.resizable(False, False)
         self.draw_buttons()
 
-    def draw_buttons(self):
+    def draw_buttons(self) -> None:
         self.select_music_button()
         self.select_playlist_button()
         self.previous_music_button()
         self.play_music_button()
         self.next_music_button()
 
-    def select_music_button(self):
+    def select_music_button(self) -> None:
         select_music_bttn = Button.make_button(self.master, "Select music", self.player.select_music)
         select_music_bttn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
-    def select_playlist_button(self):
+    def select_playlist_button(self) -> None:
         playlist_select_btn = Button.make_button(self.master, "Open playlist", self.player.create_player)
         playlist_select_btn.pack(side = tkinter.constants.TOP, anchor = tkinter.constants.CENTER)
 
-    def play_music_button(self):
+    def play_music_button(self) -> None:
         play_music_bttn = Button.make_button(self.master, "Play/Pause", self.player.play_and_pause_music)
         play_music_bttn.place(relx=0.5, rely=0.5, anchor=tkinter.constants.CENTER)
 
-    def next_music_button(self):
+    def next_music_button(self) -> None:
         next_music_btton = Button.make_button(self.master, "Next", self.player.next_music)
         next_music_btton.pack(side = tkinter.constants.RIGHT)
 
-    def previous_music_button(self):
+    def previous_music_button(self) -> None:
         previous_music_button = Button.make_button(self.master, "Previous", self.player.previous_music)
         previous_music_button.pack(side = tkinter.constants.LEFT)
 
--- scripts/player.py
+++ scripts/player.py
@@ -2,17 +2,14 @@ import tkinter
 import vlc
 import os
 
-# TODO: Talvez eu deva separar a parte de gerenciamento de playlist em outra classe e deixa a classe Player apenas para gerenciar como a música toca.
-#       Existe mais de uma responsabilidade nessa classe Player quando deveria ser apenas sobre o tocador
-
 class Playlist:
     def __init__(self):
         self.player_instance = vlc.Instance("--loop")
 
-    def get_songs_in_folder(self, folder_path) -> list:
+    def get_songs_in_folder(self, folder_path):
         try:
-            playlist_folder = os.listdir(folder_path)
-            return [os.path.join(folder_path, music) for music in playlist_folder if music.endswith(".mp3")]
+            print(f"Fetching musics from folder: {folder_path}")
+            return [os.path.join(folder_path, music) for music in os.listdir(folder_path) if music.endswith(".mp3")]
 
         except (TypeError, FileNotFoundError) as e:
             print("Insert a playlist or a valid playlist (which is a folder with .mp3 files)")
@@ -22,17 +19,18 @@ class Playlist:
     def ask_for_folder_path(self):
         return tkinter.filedialog.askdirectory()
 
-    def create_playlist(self, songs):
+    def create_playlist(self, songs) -> vlc.MediaList:
         if(songs):
             playlist = self.player_instance.media_list_new(songs) # Criando objeto do tipo MediaList (playlist)
             return playlist
+
 
 class Player:
     def __init__(self):
         self.player_instance = vlc.Instance("--loop")
         self.playlist = Playlist()
 
-    def select_music(self) -> None:
+    def select_music(self) -> vlc.MediaPlayer:
         music_path = tkinter.filedialog.askopenfilename()
 
         if not music_path.endswith(".mp3"):
@@ -41,13 +39,21 @@ class Player:
         return vlc.MediaPlayer(music_path)
 
     def create_player(self) -> vlc.MediaListPlayer:
-        self.current_folder_path = self.playlist.ask_for_folder_path()
-        songs = self.playlist.get_songs_in_folder(self.current_folder_path)
-        playlist = self.playlist.create_playlist(songs)
-        player = self.player_instance.media_list_player_new()
-        player.set_media_list(playlist)
+        try:
+            self.current_folder_path = self.playlist.ask_for_folder_path()
+            print(f"current_folder_path: {self.current_folder_path}")
 
-        return player
+            if(type(self.current_folder_path) is not tuple or self.current_folder_path != ""):
+                songs = self.playlist.get_songs_in_folder(self.current_folder_path)
+                playlist = self.playlist.create_playlist(songs)
+                player = self.player_instance.media_list_player_new()
+                player.set_media_list(playlist)
+                return player
+            raise Exception("An error occurs while trying to create a music_player")
+
+        except Exception as e: 
+            print(e)
+
 
 class MusicPlayer():
     def __init__(self):
@@ -73,14 +79,14 @@ class MusicPlayer():
         print("Selecting music")
         self.music_player = self.player.select_music()
 
-    def play_and_pause_music(self):
+    def play_and_pause_music(self) -> None:
         if(self.is_anything_playing()):
             self.music_player.pause()
         else:
             self.music_player.play()
 
-    def next_music(self):
+    def next_music(self) -> None:
         self.music_player.next()
 
-    def previous_music(self):
+    def previous_music(self) -> None:
         self.music_player.previous()
